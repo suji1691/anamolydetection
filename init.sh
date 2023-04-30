9245e74aec745e33cb1bed8ff18f3c8bc74e0092
Sujitha Sathiyamoorthy <94615232+suji1691@users.noreply.github.com>
2023-04-30 18:55:49 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt-get install bluez bluez-test-scripts python3-bluez python3-dbus libsqlite3-dev ubertooth
+
+
+sudo apt-get install ruby-dev bundler
+
+
 # clone the repository
 git clone https://github.com/pwnieexpress/blue_hydra.git
 
@@ -7,20 +13,6 @@ git clone https://github.com/pwnieexpress/blue_hydra.git
 cd blue_hydra
 
 # install dependencies
-sudo apt-get install libpcap-dev libglib2.0-dev libssl-dev sqlite3 libsqlite3-dev bluez bluez-test-scripts python-bluez python-dbus libsqlite3-dev ubertooth ruby-dev bundler
 
 bundle install
 
-#./bin/blue_hydra
-
-# build the code
-#make
-
-# install the service
-#sudo make install
-
-# enable the service
-#sudo systemctl enable blue_hydra.service
-
-# start the service
-#sudo systemctl start blue_hydra.service
